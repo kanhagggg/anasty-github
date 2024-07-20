f99f027d469971cecc4d5f295fc90c9aa372d701
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-07-20 23:13:11 +0530

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,3 +1,1 @@
-sudo apt install -y docker.io
-apt install -y docker.io
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
+bash sh -c "$(curl -fsSl https://gist.githubusercontent.com/rooted-cyber/10af5569ae3cb10af83390c8aeb4d145/raw/anastt)"
